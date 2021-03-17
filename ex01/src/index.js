19d8474ced78007c8b49f8e725e2f284fc2e7c7f
nedimhuremovic <nedimhuremovic.@gmail.com>
2021-03-17 15:04:31 +0100

    ex01 added

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+var myPet = {
+    species: "pekinezer",
+    name : "Bobi",
+    legs : 4,
+    friends : ["friend1", "friend2"]
+}
+function myFunction(myObj) {
+    return myObj;
+}
+
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = {myPet, myFunction};
